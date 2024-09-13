97a8b9a07a0f42de6d6f3f08b355eee9054da6fa
Jorni Zegers <zegers.jorni@gmail.com>
2024-09-13 15:39:08 +0200

    added source to the start.sh to fill the environment variables

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 set -e
 
 echo "run db migration"
+source /simple-bank-api/app.env
 /simple-bank-api/migrate -path /simple-bank-api/migration -database "$DB_SOURCE" -verbose up
 
 echo "start the app"
